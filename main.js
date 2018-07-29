6ceaffe872c548a481ca1a487a1974ce637ab293
Nate Wolfe <natedwolfe@gmail.com>
2018-07-29 15:06:44 -0400

    Removed unneeded semicolon

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ if('serviceWorker' in navigator) {
             .register('./sw.js', { scope: './' })
             .then(function(registration){
                 console.log("Service Worker Registered");
-            });
+            })
             .catch(function(err){
                 console.log("Service Worker failed to register", err)
             });
